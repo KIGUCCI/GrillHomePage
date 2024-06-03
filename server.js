9bd06b83182ba1739cfcebaacf6264f1e709fe66
木口 <jinkaku8@gmail.com>
2024-06-03 23:29:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+require('dotenv').config();  // .envファイルの読み込み
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// 静的ファイルの提供（public フォルダを参照せず、ファイルの実際の場所を指定）
+// 静的ファイルの提供（publicフォルダを参照）
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 環境変数をクライアントに提供するエンドポイントの定義
+app.get('/config', (req, res) => {
+  res.json({
+    applicationId: process.env.APPLICATION_ID
+  });
+});
 
 // ルートの定義
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.htmlを返す
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); // index.htmlを返す
 });
 
 // サーバーの起動
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
